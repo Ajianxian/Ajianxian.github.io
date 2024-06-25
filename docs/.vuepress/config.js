166c2a20d19bba1ee05eab23c8b4ad43bd4bbe91
Ajianxian <ajianxian@163.com>
2024-06-25 17:46:41 +0800

    init：vuepress初始化项目提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+
+  title: 'Ajianxian',
+  description: '这是我的第一个 VuePress 站点',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
